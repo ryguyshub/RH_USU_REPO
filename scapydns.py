560054a1b2b9403bc994d976e4e17b9b08950c87
ryguyshub <102702159+ryguyshub@users.noreply.github.com>
2022-04-06 12:52:51 -0600

    Update scapydns.py

--- scapydns.py
+++ scapydns.py
@@ -1,6 +1,8 @@
 from scapy.all import DNS, DNSQR, IP, sr1, UDP
+import sys
+queryhost = sys.argv[1]
 
-dnsRequest = IP(dst='8.8.8.8')/UDP(dport=53)/DNS(rd=1, qd=DNSQR(qname='www.google.com')
+dnsRequest = IP(dst='8.8.8.8')/UDP(dport=53)/DNS(rd=1, qd=DNSQR(qname=queryhost))
 dnsResponse = sr1(dnsRequest)
                                                  
 print(dnsResponse[DNS].summary())
